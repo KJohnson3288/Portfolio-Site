65a400d44ed82814da97abd4085d25eb32e037b8
KJohnson3288 <KJohnson3288@yahoo.com>
2025-05-05 22:11:59 -0400

    Mini title 300

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ let itemOne = document.getElementById("pracList");
 itemOne.textContent = "Butter Bandit";
 
 let number = 300;
+
+const miniTitle = document.getElementById("mini-title");
+miniTitle.textContent = number;
