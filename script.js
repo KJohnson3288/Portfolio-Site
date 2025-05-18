7dcdf97e27a3f5493e3c11da32be2619b6839fab
KJohnson3288 <KJohnson3288@yahoo.com>
2025-05-17 20:27:32 -0400

    portfolio site initial push

--- script.js
+++
@@ -1,8 +0,0 @@
-let itemOne = document.getElementById("pracList");
-
-itemOne.textContent = "Butter Bandit";
-
-let number = 300;
-
-const miniTitle = document.getElementById("mini-title");
-miniTitle.textContent = number;
